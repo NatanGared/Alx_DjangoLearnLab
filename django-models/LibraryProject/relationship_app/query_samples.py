f0c8a8782b2ca8b1109414c01a268712b56649ee
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-20 11:04:37 +0300

    author changed books and author to library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,23 +1,32 @@
-from relationship_app.models import Author
-from relationship_app.models import Library
-from relationship_app.models import Library
+from relationship_app.models import Author, Book, Library, Librarian
 
-author = Author.objects.get(name='Author Name')
-books_by_author = author.books.all()  
+def query_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        print(f"Books by {author_name}:")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print(f"Author '{author_name}' does not exist.")
 
-for book in books_by_author:
-    print(book.title)
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        print(f"Books in {library_name}:")
+        for book in books:
+            print(f"- {book.title}")
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
 
-library = Library.objects.get(name='library_name') 
-books_in_library = library.books.all() 
-
-for book in books_in_library:
-    print(book.title)
-
-library = Library.objects.get(name='library_name') 
-librarian = library.librarian 
-
-if librarian:
-    print(librarian.name)
-else:
-    print("No librarian assigned to this library.")
+def retrieve_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        if librarian:
+            print(f"Librarian for {library_name}: {librarian.name}")
+        else:
+            print(f"No librarian assigned to {library_name}.")
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
