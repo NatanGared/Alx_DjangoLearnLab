78cbfde29ebb8777f83ed251849e64ec97a889b9
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-20 21:49:27 +0300

    author changed books and author to library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# relationship_app/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from bookshelf.models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+from django.views.generic import DetailView
+from bookshelf.models import Book 
+
+class LibraryDetailView(DetailView):
+    model = Book
+    template_name = 'relationship_app/library_detail.html' 
+
+    def get_queryset(self):
+        library_id = self.kwargs['name']
+        return Book.objects.filter(library__id=library_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['library_name'] = "Library Name" 
+        return context
