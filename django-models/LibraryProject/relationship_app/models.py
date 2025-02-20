a81bc0b2ad29fc5e0d37ddc3978cb0295ad937f7
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-20 10:55:27 +0300

    author changed books and author to library

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -24,14 +24,14 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
-    author = models.ManyToManyField(Book, related_name='libraries')
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
     
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
-    author = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
         return self.name
