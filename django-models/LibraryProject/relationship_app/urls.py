6ca922ef659812c7a3819e0f024643ac3ae8c8b8
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-23 21:50:20 +0300

    admin based view url was updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-    path('admin/', views.admin_view, name='admin_view'),
+    path('admin/', views.Admin_view, name='admin_view'),
 ]
