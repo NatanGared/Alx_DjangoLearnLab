129941730c0aabd125a0378f6f1a5f12c6463a0a
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-02-23 21:48:41 +0300

    admin based view url was updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -71,7 +71,7 @@ def is_member(user):
 # Admin view
 @login_required
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin_view(request):
     return render(request, 'admin_view.html')
 
 # Librarian view
