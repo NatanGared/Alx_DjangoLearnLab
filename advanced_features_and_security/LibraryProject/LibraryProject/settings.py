828108826ef20c9412804b5dc78da3b72bdcb234
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-01 16:46:39 +0300

    Trial of the new record

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'relationship_app',
-    'accounts',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,3 +9,17 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author__name') 
 
 admin.site.register(Book)
+
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser 
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser 
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser , CustomUserAdmin)
