e248f7eaf57fcb3c526a602e030b5ab77401455e
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-09 22:38:29 +0300

    Authentication and permission was added

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, CustomAuthToken
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -9,4 +9,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)),
+    path('api/token-auth/', CustomAuthToken.as_view(), name='token-auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -12,3 +12,21 @@ from rest_framework import viewsets
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+#authrization with token
+# views.py
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
