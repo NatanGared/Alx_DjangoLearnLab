185768ea40e2cf81466cb70c92d7548fdda18042
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-21 11:31:11 +0300

    from django.contrib.auth.decorators import login_required included

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),  # Edit a comment
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete a comment
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),  # Edit a comment
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete a comment
 ]
