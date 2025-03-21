b6558bf575082263e63332ba7bb8e3de91c84004
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-21 10:36:04 +0300

    from django.contrib.auth.decorators import login_required included

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,3 @@
-# blog/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
@@ -31,7 +30,6 @@ def user_logout(request):
     logout(request)
     return redirect('post-list')
 
-# blog/views.py
 @login_required
 def profile(request):
     if request.method == 'POST':
@@ -39,3 +37,54 @@ def profile(request):
         request.user.save()
         return redirect('profile')  # Redirect to the profile page after update
     return render(request, 'blog/profile.html')
+
+# blog/views.py
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
+
+# List all blog posts
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# Show details of a single blog post
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# Create a new blog post
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the author to the logged-in user
+        return super().form_valid(form)
+
+# Update an existing blog post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can edit the post
+
+# Delete a blog post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'  # Redirect to the list view after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can delete the post
