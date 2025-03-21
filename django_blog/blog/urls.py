51c7553994b0c7991f65f2b02e814aa57a1b9e73
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-21 11:28:27 +0300

    from django.contrib.auth.decorators import login_required included

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,11 +20,10 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Create a new comment
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),  # Edit a comment
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),  # Edit a comment
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete a comment
 ]
