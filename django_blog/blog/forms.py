f786f912ac09c54197467a6e081fc494869bc449
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-21 12:15:30 +0300

    addition of the new files

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagField
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,7 +11,14 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class TagWidget(forms.TextInput):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.attrs.update({'placeholder': 'Add tags separated by commas'})
+
 class PostForm(forms.ModelForm):
+    tags = TagField(widget=TagWidget)  # Use your custom TagWidget
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
